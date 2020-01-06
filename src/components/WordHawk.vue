dfa15c49427ff00604cc9b341e18eec1b8a7521b
test <test@c02y80a6jgh5.fios-router.home>
2020-01-05 21:14:30 -0500

    created project and set up letter grid

---
+++ src/components/WordHawk.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="game">
+    <span
+      v-for="(letter, i) in letters"
+      :key="i"
+      :data-letter="letter"
+      :class="activeLetter(letter)"
+    >
+      {{ letter }}
+    </span>
+  </div>
+</template>
+
+<script>
+const LETTERS = 'lkashdlvkjalencfkjdhsvlnxbagsecretnujmxvkalbdfnsauytynznbcoiuyvbkiuytdtv'
+
+export default {
+  name: 'WordHawk',
+  data () {
+    return {
+      letters: LETTERS.split(''),
+      keysPressed: []
+    }
+  },
+  computed: {
+    activeLetter () {
+      return letter => {
+        const pressed = this.keysPressed.indexOf(letter) >= 0
+        return pressed ? 'active-letter' : ''
+      }
+    }
+  },
+  methods: {
+    keyPressed (e) {
+      const uniqPressed = new Set([String.fromCharCode(e.keyCode), ...this.keysPressed])
+      this.keysPressed = [...uniqPressed]
+    }
+  },
+  mounted () {
+    window.addEventListener('keypress', this.keyPressed.bind(this))
+  },
+  destroyed () {
+    window.removeEventListener('keypress', this.keyPressed)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.game {
+  height: 400px;
+  width: 400px;
+  border: 1px solid black;
+  padding: 10px;
+  display: grid;
+  grid-template-columns: repeat(9, 0.5em);
+  grid-template-rows: repeat(9, 1em);
+  grid-gap: 35px;
+
+  span {
+    text-transform: capitalize;
+    text-align: center;
+    line-height: 36px;
+    border: 1px solid black;
+    height: 40px;
+    width: 40px;
+  }
+
+  .active-letter {
+    background-color: lightblue;
+    color: red;
+    font-weight: bold;
+  }
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <WordHawk />
+  </q-page>
+</template>
+
+<script>
+import WordHawk from 'components/WordHawk'
+
+export default {
+  name: 'PageIndex',
+  components: { WordHawk }
+}
+</script>
